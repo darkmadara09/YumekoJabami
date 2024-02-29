93582f7d63ad967b853ed68d605e9b5ad5643f9b
Alpha <124826956+imr-16@users.noreply.github.com>
2024-02-29 16:56:11 +0530

    Update notify_promotion.py

--- AloneRobot/modules/notify_promotion.py
+++ AloneRobot/modules/notify_promotion.py
@@ -16,5 +16,11 @@ async def handler(_, cmu):
                     m1 = f"[{cmu.from_user.first_name}](tg://user?id={cmu.from_user.id})"
                     m2 = f"[{cmu.new_chat_member.user.first_name}](tg://user?id={cmu.new_chat_member.user.id})"
                     txt = f"{m2} was demoted by {m1}."
+        if cmu.old_chat_member.status.name != "BANNED":
+            if cmu.new_chat_member:
+                if cmu.new_chat_member.status.name == "BANNED":
+                    m1 = f"[{cmu.from_user.first_name}](tg://user?id={cmu.from_user.id})"
+                    m2 = f"[{cmu.new_chat_member.user.first_name}](tg://user?id={cmu.new_chat_member.user.id})"
+                    txt = f"{m1} banned {m2}."
     if txt:
         await _.send_message(cmu.chat.id, txt)
