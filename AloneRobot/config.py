71465dcbc7524c3aafd839fa78f36e60c7de93c3
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-03-31 13:43:51 +0530

    Update config.py

--- AloneRobot/config.py
+++ AloneRobot/config.py
@@ -23,11 +23,11 @@ class Config(object):
     # Optional fields
     CHATBOT_API="" # get it from @FallenChat_Bot using /token
     BL_CHATS = []  # List of groups that you want blacklisted.
-    DRAGONS = []  # User id of sudo users
-    DEV_USERS = []  # User id of dev users
-    DEMONS = []  # User id of support users
-    TIGERS = []  # User id of tiger users
-    WOLVES = []  # User id of whitelist users
+    DRAGONS = [6529892817]  # User id of sudo users
+    DEV_USERS = [6529892817]  # User id of dev users
+    DEMONS = [6529892817]  # User id of support users
+    TIGERS = [6529892817]  # User id of tiger users
+    WOLVES = [6529892817]  # User id of whitelist users
 
     ALLOW_CHATS = True
     ALLOW_EXCL = True
