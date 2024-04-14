cd17189b129c987d2f17f56e827a8c571a8e8081
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-04-15 00:09:17 +0530

    Update myinfo.py

--- AloneRobot/modules/myinfo.py
+++ AloneRobot/modules/myinfo.py
@@ -13,7 +13,7 @@ from AloneRobot import telethn as tgbot
 from AloneRobot.events import register 
 import AloneRobot.modules.sql.users_sql as sql
 from AloneRobot import StartTime
-import NekoRobot.utils.formatter as formatter
+import AloneRobot.utils.formatter as formatter
 
 
 edit_time = 5
