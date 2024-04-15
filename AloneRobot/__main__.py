c321e3d59b503583acefa5c010c632cc53830b69
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-04-15 13:47:53 +0530

    Update __main__.py

--- AloneRobot/__main__.py
+++ AloneRobot/__main__.py
@@ -218,9 +218,9 @@ def start(update: Update, context: CallbackContext):
                 PM_START_TEX.format(usr.first_name), parse_mode=ParseMode.MARKDOWN
             )
             time.sleep(0.4)
-            lol.edit_text("ɪs..")
-            time.sleep(0.5)
             lol.edit_text("ʏᴜᴍᴇᴋᴏ.")
+            time.sleep(0.5)
+            lol.edit_text("ɪs..")
             time.sleep(0.3)
             lol.edit_text("ꜱᴛᴀʀᴛɪɴɢ...")
             time.sleep(0.4)
